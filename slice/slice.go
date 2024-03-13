56cf8efef0d4c4731fa56a5a9fd1acdba54f0007
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-03-13 22:11:35 +0800

    refactor: slice map

--- slice/slice.go
+++ slice/slice.go
@@ -93,10 +93,10 @@ func Unique[T lib.Ordered](nums []T) []T {
 	return newNums
 }
 
-func Map[Src any, Dst any](src []Src, m func(idx int, src Src) Dst) []Dst {
+func Map[Src any, Dst any](src []Src, m func(src Src) Dst) []Dst {
 	dst := make([]Dst, len(src))
 	for i, s := range src {
-		dst[i] = m(i, s)
+		dst[i] = m(s)
 	}
 	return dst
 }
